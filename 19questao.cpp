54965dae68a67d960e0155ae841bc285a24b1fd7
geovana707 <geovana115257@gmail.com>
2025-07-09 16:56:36 -0300

    Update 19questao.cpp

--- 19questao.cpp
+++ 19questao.cpp
@@ -1,22 +1,20 @@
+// Example program
 #include <iostream>
+#include <string>
 using namespace std;
-  void media (float  n1,float n2,float n3){
-  float media;
-      media = (n1+n2+n3)/3;
-      
-      cout<<"A media do aluno é "<<media<<endl;
-  }
 
-int main() {
-   float nota1,nota2,nota3;
-   
-   cout<<"Informe a primeira nota: "<<endl;
-   cin>>nota1;
-   cout<<"Informe a segunda nota: "<<endl;
-   cin>>nota2;
-   cout<<"Informe a terceira nota: "<<endl;
-   cin>>nota3;
-   
-   media (nota1, nota2, nota3);
-    return 0;
+float media(float num1, float num2, float  num3){
+    float med = (num1 + num2 + num3) / 3;
+    return med;
+}
+
+int main()
+{
+    float n1;
+    float n2;
+    float n3;
+    cout << "digite o valor das suas tres notas: ";
+    cin >> n1 >> n2 >> n3;
+    int res = media(n1, n2, n3);
+    cout << "A sua media e: " << res << endl;
 }
