c2d6bf087c8bbe4127f41b289418b82c160d52a6
geovana707 <geovana115257@gmail.com>
2025-07-09 16:50:04 -0300

    Update 14questao.cpp

--- 14questao.cpp
+++ 14questao.cpp
@@ -1,14 +1,19 @@
+
 #include <iostream>
 using namespace std;
 
-void exibirVetor(int* v, int tamanho) {
-    for (int i = 0; i < tamanho; i++)
-        cout << v[i] << " ";
-    cout << endl;
+// Função que recebe um ponteiro para vetor de inteiros
+void exibirVetor(int* vetor, int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        cout << "Elemento " << i << ": " << *(vetor + i) << endl;
+    }
 }
 
 int main() {
-    int vetor[5] = {1, 2, 3, 4, 5};
-    exibirVetor(vetor, 5);
+    int numeros[5] = {10, 20, 30, 40, 50};
+
+    // Passa o endereço do primeiro elemento (ou só o nome do vetor)
+    exibirVetor(numeros, 5);
+
     return 0;
 }
