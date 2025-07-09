6eaafe96509623c51cd4ec8e229066e19b772dcc
geovana707 <geovana115257@gmail.com>
2025-07-09 16:49:05 -0300

    Update 13questao.cpp

--- 13questao.cpp
+++ 13questao.cpp
@@ -1,22 +1,19 @@
+// Example program
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    int subst = 0;
-    int a;
-    int *ptr1=&a;
-    int b;
-    int *ptr2=&b;
-    
-    cout<<"Informe o primeiro valor: "<<endl;
-    cin>>a;
-    cout<<"Informe o segundo valor: "<<endl;
-    cin>>b;
-    cout<<"Os valores fornecidos sao os seguintes: primeiro valor e "<<a<<" o segundo valor e "<<b<<endl;
-    subst = *ptr1;
-    *ptr1 = *ptr2;
-    *ptr2 = subst;
-    
-    cout<<"Apos a troca os valores fornecidos sao: o primeiro ficou como "<<*ptr1<<" e o segundo como "<< *ptr2<<endl;
-    return 0;
+
+
+int main()
+{
+  int num1;
+  int num2;
+  cout << "Digite dois valores inteiros: ";
+  cin >> num1 >> num2;
+  int *ptr1 = &num1;
+  int *ptr2 = &num2;
+  *ptr1 = 20;
+  *ptr2 = 400;
+  cout << "Os valores trocado: " << num1 << " o segundo  " << num2 << endl;
 }
