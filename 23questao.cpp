e94cf059fa8529c74fa5e1b8b1606069dd715d88
geovana707 <geovana115257@gmail.com>
2025-07-09 21:38:05 -0300

    Update 23questao.cpp

--- 23questao.cpp
+++ 23questao.cpp
@@ -1,9 +1,14 @@
-#pragma once
-#include <iostream>
-using namespace std;
+#ifndef FUNCOES_H
+#define FUNCOES_H
 
+#include <iostream>
+
+// Função que mostra informações básicas
 void mostrarInfo() {
-    cout << "Informacoes do programa." << endl;
-    return 0;
+    std::cout << "Informacoes do Sistema: \n";
+    std::cout << "Versao: 1.0.0\n";
+    std::cout << "Autor: Geovana\n";
+    std::cout << "Modulo: Demonstracao de import guards\n";
 }
 
+#endif // FUNCOES_H
